ea4fb384ea9a07bb8a2fddc0a4293a619a1595e4
jirokichi <topaz30th.kdy@gmail.com>
2015-09-27 08:33:02 +0000

    seventh commit

--- quickstart.js
+++ quickstart.js
@@ -47,67 +47,66 @@ var sockets = [];
 
 io.on('connection', function (socket) {
     
-    socket.on("emit_from_client", function(data){
-      console.log("初めての"+ data.userName + "のsoket.io:" + data.msg);
-      var jsonfileName = jsonNumber + '.json';
-      // var outputPath = './public/' + jsonfileName;
-      // fs.writeFile(outputPath, JSON.stringify(data, null, '    '));
-      
-      myGoogleDrive.jsonFile = JSON.stringify(data, null, '    ');//imageFile;
-      myGoogleDrive.jsonFileName = jsonfileName;
-      myGoogleDrive.authorize(onStartUploadJSONFileToGoogleDrive);
-      function onStartUploadJSONFileToGoogleDrive(auth) {
-        console.log('~~~~ Start startInitialCallForGoogleDrive');
-        myGoogleDrive.insertJson(auth, function () {
-          socket.emit("emit_from_server", "uplaoded:" + jsonfileName);
-        });
-        console.log('~~~~ Finish startInitialCallForGoogleDrive');
-        jsonNumber++;
-      }
-    });
-
-    socket.on('upload_from_client', function (data) {
-      console.log("upload from client");
-      var name = data.name;
-      var img = data.data;
-      console.log("upload from client:" + name);
-      console.log("upload from client:" + img);
-      var imgStructure = img.split(',');
-      var base64 = imgStructure[1];
-  
-      if (!base64) {
-          console.log('fal to parse image');
-      }
-  
-      // 保存するならば
-      // var now = new Date().getTime();
-      // var outputPath = './public/' + now + name;
-  
-      // fs.writeFile(
-      //     outputPath,
-      //     new Buffer(base64, 'base64'),
-      //     function (err) {
-      //       　console.log("fs.writeFile")
-      //         console.log(err);
-      //     }
-      // );
+  socket.on("emit_from_client", function(data){
+    console.log("クライアント「"+ data.userName + "」から次のデータ受信:" + data.msg);
+    var jsonfileName = jsonNumber + '.json';
+    // var outputPath = './public/' + jsonfileName;
+    // fs.writeFile(outputPath, JSON.stringify(data, null, '    '));
     
-      myGoogleDrive.imageFile = new Buffer(base64, 'base64');//imageFile;
-      myGoogleDrive.imageFileName = name;
-      console.log("authorize:%s", myGoogleDrive.imageFileName);
-      myGoogleDrive.authorize(onStartUploadFileToGoogleDrive);
-      function onStartUploadFileToGoogleDrive(auth) {
-        console.log('~~~~ Start startInitialCallForGoogleDrive');
-        myGoogleDrive.insertImage(auth, function () {
-          socket.emit("upload_from_server", "uplaoded:" + name);
-        });
-        console.log('~~~~ Finish startInitialCallForGoogleDrive');
-      }
-      // もしアップロード終了時に合図が欲しいならば
-      // socket.emit('notify', {url : publicUrl});
-    });
-    
+    myGoogleDrive.jsonFile = JSON.stringify(data, null, '    ');//imageFile;
+    myGoogleDrive.jsonFileName = jsonfileName;
+    myGoogleDrive.authorize(onStartUploadJSONFileToGoogleDrive);
+    function onStartUploadJSONFileToGoogleDrive(auth) {
+      console.log('~~~~ Start startInitialCallForGoogleDrive');
+      myGoogleDrive.insertJson(auth, function () {
+        socket.emit("emit_from_server", "uplaoded:" + jsonfileName);
+      });
+      console.log('~~~~ Finish startInitialCallForGoogleDrive');
+      jsonNumber++;
+    }
   });
+
+  socket.on('upload_from_client', function (data) {
+    console.log("クライアントから画像情報の受信：");
+    var name = data.name;
+    var img = data.data;
+    console.log("upload from client:" + name);
+    console.log("upload from client:" + img);
+    var imgStructure = img.split(',');
+    var base64 = imgStructure[1];
+
+    if (!base64) {
+        console.log('fal to parse image');
+    }
+
+    // 保存するならば
+    // var now = new Date().getTime();
+    // var outputPath = './public/' + now + name;
+
+    // fs.writeFile(
+    //     outputPath,
+    //     new Buffer(base64, 'base64'),
+    //     function (err) {
+    //       　console.log("fs.writeFile")
+    //         console.log(err);
+    //     }
+    // );
+  
+    myGoogleDrive.imageFile = new Buffer(base64, 'base64');//imageFile;
+    myGoogleDrive.imageFileName = name;
+    console.log("authorize:%s", myGoogleDrive.imageFileName);
+    myGoogleDrive.authorize(onStartUploadFileToGoogleDrive);
+    function onStartUploadFileToGoogleDrive(auth) {
+      console.log('~~~~ Start startInitialCallForGoogleDrive');
+      myGoogleDrive.insertImage(auth, function () {
+        // アップロード完了の合図を送信
+        socket.emit("upload_from_server", "uplaoded:" + name);
+      });
+      console.log('~~~~ Finish startInitialCallForGoogleDrive');
+    }
+  });
+  
+});
 
 /**
  * 画像アップロードのための認証後に呼び出されるメソッド
